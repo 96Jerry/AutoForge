242d32c2b8460a2d3d65585e4788ef685140fcc4
Jay <jay@madsq.net>
2025-03-17 09:21:48 +0900

    feat: 클릭시 디스코드 앱 open

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { exec } from 'child_process';
 import * as notifier from 'node-notifier';
 import { CrawlService } from 'src/crawl/crawl.service';
 import { GoogleService } from 'src/google/google.service';
@@ -18,7 +19,7 @@ export class CronService {
   }
 
   // 매일 | 9시
-  @Cron('0 0 9 * * *')
+  @Cron('0 * 9 * * *')
   async sendVacationEmail(): Promise<void> {
     return this.googleService.sendVacationEmail();
   }
@@ -31,12 +32,24 @@ export class CronService {
         title: '커스텀 알람',
         message: '디스코드 알림 확인 필요!',
         closeLabel: '닫기',
+        wait: true,
         timeout: 3600,
       },
       function (err, res, meta) {
         console.log(res, meta);
       },
     );
+
+    notifier.on('click', () => {
+      exec('open /Applications/Discord.app', (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        console.log('App opened');
+      });
+    });
   }
 
   // 매주 | 월~금 | 9시,14시 | 정각
